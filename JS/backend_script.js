27e809a60fbcdf4285532e46f3cb6fe941da80ee
chavangaurav <chavangaurav@hotmail.com>
2017-05-22 21:21:43 +0530

    header image resoustion changed

--- JS/backend_script.js
+++ JS/backend_script.js
@@ -1,14 +1,22 @@
-$(document).ready(function() {
-            function toggle_visibility(id) {
-                var e = document.getElementById(id);
-                if (e.style.display == 'block') e.style.display = 'none';
-                else e.style.display = 'block';
-            }
+$(document).ready(function () {
+    function toggle_visibility(id) {
+        var e = document.getElementById(id);
+        if (e.style.display == 'block') e.style.display = 'none';
+        else e.style.display = 'block';
+    }
 
-            $('#open_category').click(function(){
-                toggle_visibility('category');
-            });
-            $('#close_category').click(function(){
-                toggle_visibility('category');
-            });
+    $('#open_category').click(function () {
+        toggle_visibility('category').fadeIn(2000);
+    });
+    $('#close_category').click(function () {
+        toggle_visibility('category');
+    });
+
+    $('#close_user').click(function () {
+        toggle_visibility('user');
+    });
+
+    $('#open_user').click(function () {
+        toggle_visibility('user');
+    });
 });
